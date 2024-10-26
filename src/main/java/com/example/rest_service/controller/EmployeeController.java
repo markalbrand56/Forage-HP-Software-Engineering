3cb257b46dbb11dcf1c819c65087b834467c8580
Mark Albrand <mark.albrand5@gmail.com>
2024-10-25 23:15:55 -0600

    Task 3: Creating employees

--- src/main/java/com/example/rest_service/controller/EmployeeController.java
+++ src/main/java/com/example/rest_service/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.rest_service.controller;
 
+import com.example.rest_service.model.Employee;
 import com.example.rest_service.model.Employees;
 import com.example.rest_service.service.EmployeeManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +20,11 @@ public class EmployeeController {
     public Employees getEmployees() {
         return employeeManager.getEmployees();
     }
+
+    @PostMapping("/employees")
+    public String addEmployee(@RequestBody Employee employee) {
+        employeeManager.addEmployee(employee);
+        return "Employee added successfully!";
+    }
+
 }
