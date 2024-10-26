d2398583f66275f95cb734d2b1f730e0f4231cdd
Mark Albrand <mark.albrand5@gmail.com>
2024-10-25 22:52:04 -0600

    Spring Boot setup

---
+++ src/main/java/com/example/rest_service/model/Employee.java
@@ -0,0 +1,29 @@
+package com.example.rest_service.model;
+
+public class Employee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String title;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
